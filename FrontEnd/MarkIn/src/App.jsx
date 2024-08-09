1043e5725ab665e825490b754f7693555b66c4e6
Maryam Saba <maryams91101@gmail.com>
2024-08-09 14:22:06 +0500

    Client Authentication Integrated

--- FrontEnd/MarkIn/src/App.jsx
+++ FrontEnd/MarkIn/src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
-
+import { Route,Routes } from "react-router-dom";
 import LogIn from "./Pages/Login";
+import UserAuth from "./Pages/UserAuth";
+import Register from "./Pages/Register"
 
 function App() {
   return (
     <>
      
-      <LogIn/>
+      {/* <LogIn/> */}
+      <Routes>
+        <Route path="/UserAuth" element = {<UserAuth/>}>
+          <Route path="Register" element = {<Register/>} />
+          <Route path="Login" element = {<LogIn/>}/>
+        </Route>
+      </Routes>
     </>
   );
 }
