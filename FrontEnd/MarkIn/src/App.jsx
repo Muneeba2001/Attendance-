51b890c073ae240c95acf592b92c4b2097dd99c9
Muneeba2001 <muneebawaseem1006@gmail.com>
2024-08-09 14:37:23 +0500

    Register Schema Integrated

--- FrontEnd/MarkIn/src/App.jsx
+++ FrontEnd/MarkIn/src/App.jsx
@@ -1,18 +1,17 @@
 import "./App.css";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LogIn from "./Pages/Login";
 import UserAuth from "./Pages/UserAuth";
-import Register from "./Pages/Register"
+import Register from "./Pages/Register";
 
 function App() {
   return (
     <>
-     
       {/* <LogIn/> */}
       <Routes>
-        <Route path="/UserAuth" element = {<UserAuth/>}>
-          <Route path="Register" element = {<Register/>} />
-          <Route path="Login" element = {<LogIn/>}/>
+        <Route path="/UserAuth" element={<UserAuth />}>
+          <Route path="Register" element={<Register />} />
+          <Route path="Login" element={<LogIn />} />
         </Route>
       </Routes>
     </>
