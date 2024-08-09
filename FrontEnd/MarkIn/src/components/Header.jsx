9c8df1d906a91a688bda03c44bbe2ed07383bd86
Maryam Saba <maryams91101@gmail.com>
2024-08-09 16:21:53 +0500

    Header Updation

--- FrontEnd/MarkIn/src/components/Header.jsx
+++ FrontEnd/MarkIn/src/components/Header.jsx
@@ -4,9 +4,14 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
 return(
-    <header>
-    <nav className="bg-black w-full h-[80px]">
+    <header className="flex  justify-between items-center bg-black w-full p-4">
+         <h1 className="text-white">Logo</h1>
+    <nav className="  text-white  p-4 space-x-6">
+   
         <NavLink to="/">Home</NavLink>
+        <NavLink to="/">AboutUs</NavLink>
+        <NavLink to="/">Gallery</NavLink>
+        <NavLink to="/">FAQ</NavLink>
     </nav>
 </header>
 )
